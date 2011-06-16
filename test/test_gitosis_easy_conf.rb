bf4110fa814f391dcd929a75e9f1bea6d0b6c608
Gerrit Riessen <gerrit.riessen@gmail.com>
2011-06-16 07:34:41 +0200

    Initial commit to gitosis_easy_conf.

---
+++ test/test_gitosis_easy_conf.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestGitosisEasyConf < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
